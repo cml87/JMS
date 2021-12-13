586814ccd1ee72ff06c054e5b39ca4e99003394b
camilo.perez <camilo.perez@kaleyra.com>
2021-12-13 18:09:25 +0100

    delete souts

--- src/main/java/com/example/jms/messagestructure/MessageTypesDemo.java
+++ src/main/java/com/example/jms/messagestructure/MessageTypesDemo.java
@@ -14,30 +14,22 @@ import java.util.Map;
 public class MessageTypesDemo {
    public static void main(String[] args) throws NamingException, InterruptedException, JMSException {
 
-       System.out.println("-1");
-
        // get the reference to the root context of the JNDI tree
        // This will read the properties file
        InitialContext initialContext = new InitialContext();
        Queue queue = (Queue) initialContext.lookup("queue/myQueue");
-       System.out.println("0");
 
        // JMSContext will have the Connection and the Session
        // I think this is either using defaults or properties from jndi.properties file
        try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
                 JMSContext jmsContext = cf.createContext()) {
 
-           System.out.println("dfd");
-
            JMSProducer producer = jmsContext.createProducer();
-
-           System.out.println("a");
 
            BytesMessage bytesMessage = jmsContext.createBytesMessage();
            bytesMessage.writeUTF("John"); // first payload ?
            bytesMessage.writeLong(123l);  // second payload ?
            //producer.send(queue, bytesMessage);
-           System.out.println("b");
 
           // BytesMessage messageReceived = (BytesMessage) jmsContext.createConsumer(queue).receive();
           // System.out.println(messageReceived.readUTF());
@@ -51,8 +43,6 @@ public class MessageTypesDemo {
 //           StreamMessage messageReceived = (StreamMessage) jmsContext.createConsumer(queue).receive();
 //           System.out.println(messageReceived.readBoolean());
 //           System.out.println(messageReceived.readFloat());
-           System.out.println("c");
-
 
            MapMessage mapMessage = jmsContext.createMapMessage();
            mapMessage.setBoolean("isCreditAvailable", true);
