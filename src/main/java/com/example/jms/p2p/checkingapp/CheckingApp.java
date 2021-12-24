c8a5c518513934049284fdb312658c9e6b1cb0a4
camilo.perez <camilo.perez@kaleyra.com>
2021-12-24 18:21:41 +0100

    bootstrap response

--- src/main/java/com/example/jms/p2p/checkingapp/CheckingApp.java
+++ src/main/java/com/example/jms/p2p/checkingapp/CheckingApp.java
@@ -17,58 +17,59 @@ public class CheckingApp {
 
         InitialContext initialContext = new InitialContext();
         Queue requestQueue = (Queue) initialContext.lookup("queue/requestQueue");
+        Queue replyQueue = (Queue) initialContext.lookup("queue/replyQueue");
+
 
         // the reply queue will be a temporary queue!
 
         try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
              JMSContext jmsContext = cf.createContext()) {
 
-            Queue replyQueue = jmsContext.createTemporaryQueue();
-
             Person person = new Person(123, "John", "Smith", LocalDate.parse("1990-01-23"), "+01 7896787788", "pepe@gmail.com");
+        //    Queue replyQueue = jmsContext.createTemporaryQueue();
 
+
+            JMSProducer producer = jmsContext.createProducer();
+            //TextMessage message = jmsContext.createTextMessage("Arise awake and stop not till the goal is reached");
             ObjectMessage message = jmsContext.createObjectMessage();
-            message.setObject(person);
             message.setJMSReplyTo(replyQueue);
+
+            message.setObject(person);
+
+
+            message.setJMSReplyTo(replyQueue);
+            producer.send(requestQueue, message);
+            System.out.printf("MessageId sent by producer: [%s] \n", message.getJMSMessageID());
 
             Map<String, ObjectMessage> messages = new HashMap<>();
             messages.put(message.getJMSMessageID(), message);
 
-            JMSProducer producer = jmsContext.createProducer();
-            System.out.println("message is: "+ message.toString());
-            System.out.println("message id is: "+message.getJMSMessageID());
-            producer.send(requestQueue, message);
+
+            JMSConsumer consumer = jmsContext.createConsumer(requestQueue);
+            ObjectMessage message1 = (ObjectMessage) consumer.receive();
+            System.out.println("id of message received: "+ message1.getJMSMessageID());
 
 
-//            JMSConsumer consumer = jmsContext.createConsumer(requestQueue);
-//            ObjectMessage message1 = (ObjectMessage) consumer.receive();
-//            Person person1 = (Person) message1.getObject();
-//
-//            MapMessage response = jmsContext.createMapMessage();
-//            response.setJMSCorrelationID(message1.getJMSMessageID());
-//            response.setBoolean("isReservationDone", true);
-//            jmsContext.createProducer().send(message1.getJMSReplyTo(), response);
-//
-//
-//            System.out.println("received person is: "+ person1.toString());
+            Person person1 = (Person) message1.getObject();
+            System.out.println("Person received: "+ person1.toString());
 
 
+            MapMessage response = jmsContext.createMapMessage();
+            response.setJMSCorrelationID(message1.getJMSMessageID());
+            response.setBoolean("isReservationDone", true);
 
+            jmsContext.createProducer().send(message1.getJMSReplyTo(), response);
 
+            //MapMessage reply = (MapMessage) jmsContext.createConsumer(replyQueue).receive();
 
+            //System.out.println("correlation id in reply: "+ reply.getJMSCorrelationID());
+            //System.out.println("reply message: isReservationDone:"+ reply.getBoolean("isReservationDone"));
 
 
         }
 
 
-
-
     }
-
-
-
-
-
 
 
 }
